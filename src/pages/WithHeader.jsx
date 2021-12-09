5dcaf0c8580904b93338b68196ad3ddb6db0a604
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-12-08 20:15:00 -0400

    signals what is to be copied

--- src/pages/WithHeader.jsx
+++ src/pages/WithHeader.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import mainContext from '../contexts/mainContext';
 
+// Função que altera o nome da página no contexto
 import { getPageName } from '../helpers';
 import Header from '../components/Header';
 
+// Pegar o location para mandar para getPageName
 export default function WithHeader({ location }) {
+  // Copiar esse acesso ao contexto
   const { setPageName } = useContext(mainContext);
 
-  // Pega o nome da pagina na URL e lança no contexto
+  // Pega o nome da pagina atual na URL e lança no contexto
   useEffect(() => {
     getPageName(location.pathname, setPageName);
   }, [location.pathname, setPageName]);
