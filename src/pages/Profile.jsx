6dfd029f45244ab22136e06d67cf137e63e3f981
Paola Gay <paolagaydossantos@gmail.com>
2022-01-06 21:53:42 +0100

    Fix 82-87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,50 +1,47 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import React, { useEffect, useState, useContext } from 'react';
 import DefaultLayout from '../components/DefaultLayout';
+import mainContext from '../contexts/mainContext';
 
 export default function Profile({ history }) {
   const [user, setUser] = useState('');
+  const { isMounted } = useContext(mainContext);
 
   useEffect(() => {
-    const { email } = JSON.parse(localStorage.getItem('user'));
-    setUser(email);
+    const userOne = JSON.parse(localStorage.getItem('user'));
+    if (userOne) {
+      setUser(userOne.email);
+    }
   }, []);
 
-  return (
-    <div>
-      <DefaultLayout pathname="/perfil">
-        <Header title="Perfil" />
-        Perfil
-        <p data-testid="profile-email">{user}</p>
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-          onClick={ () => history.push('/receitas-feitas') }
-        >
-          Receitas Feitas
-        </button>
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-          onClick={ () => history.push('/receitas-favoritas') }
-        >
-          Receitas Favoritas
-        </button>
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => {
-            localStorage.clear();
-            history.push('/');
-          } }
-        >
-          Sair
-        </button>
-        <Footer />
-      </DefaultLayout>
-    </div>
+  return isMounted && (
+    <DefaultLayout pathname="/perfil">
+      <p data-testid="profile-email">{user}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/receitas-feitas') }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Sair
+      </button>
+    </DefaultLayout>
   );
 }
 
